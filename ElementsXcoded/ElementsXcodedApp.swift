27a5b96fe673a363c745f7cc1a987a7c8292ef74
stannyu <stas.yudzitski@yandex.ru>
2022-10-10 15:55:45 +0200

    Initial Commit

---
+++ ElementsXcoded/ElementsXcodedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsXcodedApp.swift
+//  ElementsXcoded
+//
+//  Created by Stas Yudzitski on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElementsXcodedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
